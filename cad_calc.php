f740e8e99e63f89a807b8a3c06c1d8f821cd4434
Gabriel Lima <gabrielveigalima@icloud.com>
2019-04-18 11:38:57 -0300

    verificando se o usuário já usou a calculadora

--- cad_calc.php
+++ cad_calc.php
@@ -1,34 +1,35 @@
 <?php
 
+session_start();
 
 include_once('connect/connect.php');
 
+//Tratando dados do formulário
+
 if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['name_company']) && isset($_POST['destination']) && isset($_POST['category']) && isset($_POST['subcategory']) && isset($_POST['amount'])){
 
-	$name = mysqli_escape_string($conn, $_POST['name']);
-	echo $name;
+	$name 			= mysqli_escape_string($conn, $_POST['name']);
+	$email 			= mysqli_escape_string($conn, strtolower ($_POST['email']));
+	$tel 			= mysqli_escape_string($conn, $_POST['tel']);
+	$name_company 	= mysqli_escape_string($conn, $_POST['name_company']);
+	$destination 	= mysqli_escape_string($conn, $_POST['destination']);
+	$category 		= mysqli_escape_string($conn, $_POST['category']);
+	$subcategory 	= mysqli_escape_string($conn, $_POST['subcategory']);
+	$amount 		= mysqli_escape_string($conn, $_POST['amount']);
 
-	$email = mysqli_escape_string($conn, strtolower ($_POST['email']));
-	echo $email;
 
-	$tel = mysqli_escape_string($conn, $_POST['tel']);
-	echo $tel;
-
-	$name_company = mysqli_escape_string($conn, $_POST['name_company']);
-	echo $name_company;
-
-	$destination = mysqli_escape_string($conn, $_POST['destination']);
-	echo $destination;
-
-	$category = mysqli_escape_string($conn, $_POST['category']);
-	echo $category;
-
-	$subcategory = mysqli_escape_string($conn, $_POST['subcategory']);
-	echo $subcategory;
-
-	$amount = mysqli_escape_string($conn, $_POST['amount']);
-	echo $amount;
+	$query = mysqli_query($conn, "SELECT id 
+								FROM users 
+								WHERE email = '$email'") or die(mysqli_error($conn));
+	$num_rows = mysqli_num_rows($query);
+	if ($num_rows > 0){
+		echo "cadastrado";
+	}else{
+		echo "não cadastrado";
+	}
 	
 } else {
 	echo "Preencha todos os campos!";
+	header('Location:index.php');
 } 
+
