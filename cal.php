1e6742a92f5f7607f937d7bf8a79f4449c729c81
Gabriel Lima <gabrielveigalima@icloud.com>
2019-04-22 15:05:12 -0300

    melhorando a confirmação

--- cal.php
+++ cal.php
@@ -2,14 +2,19 @@
 
 session_start();
 
-if isset($_GET['confirm'] == 'ok'){
+if (isset($_GET['confirm']){
 
-	header("Location:calcular.php");
+	if ($_GET['confirm'] == 'ok'){
+
+		$_SESSION['preencher'] = 1;
+
+		header("Location:calcular.php");
   
-} else {
-	$_SESSION['aviso'] = "<div class='alert alert-danger' role='alert'>
-  Preencha o formulário para calcular o valor seu resíduo!
-</div>";
+	} else {
+		$_SESSION['aviso'] = "<div class='alert alert-danger' role='alert'>
+	  Preencha o formulário para calcular o valor seu resíduo!
+	</div>";
+	}
 }
 
 
