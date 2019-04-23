573f26da5b434f827b46dfce839863a7d6cf7173
Gabriel Lima <gabrielveigalima@icloud.com>
2019-04-23 15:44:36 -0300

    calculando o valor do resíduo

--- relatorio.php
+++ relatorio.php
@@ -15,42 +15,12 @@ if (isset($_POST['subcategory']) && isset($_POST['amount'])){
 	$subcategory 	= mysqli_escape_string($conn, $_POST['subcategory']);
 	$amount 		= mysqli_escape_string($conn, $_POST['amount']);
 
-	$query = mysqli_query($conn, "SELECT price FROM sub_category WHERE id = ".$subcategory."") or die(mysqli_error($conn));
+	$query = mysqli_query($conn, "SELECT name, price FROM sub_category WHERE id = ".$subcategory."") or die(mysqli_error($conn));
     $resultado = mysqli_fetch_assoc($query);
 
     $valor = $resultado['price'] * $amount;
 	
+    echo "O potencial valor para o resíduo ".$resultado['name']." é R$ ".$valor.".";
 
-	
-	$html = '<p>';	
-	$html .= $valor;
-	
-	$html .= '</p>';
-
-	
-	//referenciar o DomPDF com namespace
-	use Dompdf\Dompdf;
-
-	// include autoloader
-	require_once("dompdf/autoload.inc.php");
-
-	//Criando a Instancia
-	$dompdf = new DOMPDF();
-	
-	// Carrega seu HTML
-	$dompdf->load_html('
-			<h1 style="text-align: center;">Celke - Relatório de Transações</h1>
-			'. $html .'
-		');
-
-	//Renderizar o html
-	$dompdf->render();
-
-	//Exibibir a página
-	$dompdf->stream(
-		"relatorio_celke.pdf", 
-		array(
-			"Attachment" => false //Para realizar o download somente alterar para true
-		)
-	);
+}
 ?>
