6aa0dde86d8868141e6203be7257d41cb2b33e3f
Gabriel Lima <gabrielveigalima@icloud.com>
2019-04-23 11:24:48 -0300

    gerando relatório

--- relatorio.php
+++ relatorio.php
@@ -19,9 +19,38 @@ if (isset($_POST['subcategory']) && isset($_POST['amount'])){
     $resultado = mysqli_fetch_assoc($query);
 
     $valor = $resultado['price'] * $amount;
+	
 
-    echo $valor;
+	
+	$html = '<p>';	
+	$html .= $valor;
+	
+	$html .= '</p>';
 
+	
+	//referenciar o DomPDF com namespace
+	use Dompdf\Dompdf;
 
+	// include autoloader
+	require_once("dompdf/autoload.inc.php");
 
-}
+	//Criando a Instancia
+	$dompdf = new DOMPDF();
+	
+	// Carrega seu HTML
+	$dompdf->load_html('
+			<h1 style="text-align: center;">Celke - Relatório de Transações</h1>
+			'. $html .'
+		');
+
+	//Renderizar o html
+	$dompdf->render();
+
+	//Exibibir a página
+	$dompdf->stream(
+		"relatorio_celke.pdf", 
+		array(
+			"Attachment" => false //Para realizar o download somente alterar para true
+		)
+	);
+?>
