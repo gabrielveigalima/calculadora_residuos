32b9c57e4b4994a4613ded3a6005dc00dcef1ec4
Gabriel Lima <gabrielveigalima@icloud.com>
2019-04-24 12:51:17 -0300

    redirecionando para index

--- relatorio.php
+++ relatorio.php
@@ -63,5 +63,13 @@ if (isset($_POST['subcategory']) && isset($_POST['amount'])){
 		)
 	);
 
+} 
+
+else {
+    $_SESSION['aviso'] = "<div class='alert alert-danger' role='alert'>    Preencha o formulário para calcular o valor seu resíduo!  </div>";
+
+  header("Location: index.php");
+  
 }
+
 ?>
