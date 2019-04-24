776aab14a87961a860326fe1ba971d45d8cd7b7b
Gabriel Lima <gabrielveigalima@icloud.com>
2019-04-24 10:36:17 -0300

    add texto final resultado

--- relatorio.php
+++ relatorio.php
@@ -20,7 +20,9 @@ if (isset($_POST['subcategory']) && isset($_POST['amount'])){
 
     $valor = $resultado['price'] * $amount;
 	
-    echo "O potencial valor para o resíduo ".$resultado['name']." é R$ ".$valor.".";
+    echo "Os resíduos plásticos de ".$resultado['name']." gerados tem potencial para retornar R$".$valor." mensalmente para sua empresa.<br><br>Você pode transformar este potencial em realidade através da Plataforma da Polen . A Polen é uma plataforma online que conecta empresas que geram resíduos com empresas que compram resíduos, em todo o Brasil. Através dela, sua empresa pode vender seus resíduos de plástico, começar a cortar custos e fazer receita extra. <a target='_blanck' href= 'https://www.brpolen.com.br/'>Cadastre-se gratuitamente aqui!</a>";
+
+
 
 }
 ?>
